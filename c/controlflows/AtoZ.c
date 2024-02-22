2ad50194109a0f2e8af0def1a5f779bc6036acf6
KotariVinay <vinaykotari7@gmail.com>
2024-02-22 23:31:02 +0530

    controlflows

---
+++ c/controlflows/AtoZ.c
@@ -0,0 +1,17 @@
+/*
+Name : K.Y.Naga Sai Vinay
+Date : 22-02-2024
+Description : WAP to print alphabets from A To Z.
+Sample input :
+Sample output :
+*/
+#include<stdio.h>
+int main()
+{
+    for(int i=65;i<91;i++)
+    {
+	printf("%c ",i);
+    }
+        return 0;
+}
+
