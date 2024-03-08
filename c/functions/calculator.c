f1d6ed091823f211623c922445a2ceaa6d509cfe
KotariVinay <vinaykotari7@gmail.com>
2024-03-08 23:05:58 +0530

    functions

---
+++ c/functions/calculator.c
@@ -0,0 +1,47 @@
+/*
+Name :
+Date :
+Description :
+Sample input :
+Sample output :
+*/
+// C Program to make a Simple Calculator
+// Using switch case
+// C Program to make a Simple Calculator
+// Using switch case
+#include <stdio.h>
+#include <stdlib.h>
+
+// Driver code
+int main()
+{
+    char ch;
+    double a, b;
+    while (1) {
+	printf("Enter an operator (+, -, *, /)if want to exit press x: ");
+	scanf(" %c", &ch);
+	if (ch == 'x')
+	    exit(0);
+	printf("Enter two first and second operand: ");
+	scanf("%lf %lf", &a, &b);
+	switch (ch) {
+     case '+':	
+	 printf("%.1lf + %.1lf = %.1lf\n", a, b, a + b);
+	 break;	
+     case '-':
+	 printf("%.1lf - %.1lf = %.1lf\n", a, b, a - b);
+	 break;
+     case '*':
+	 printf("%.1lf * %.1lf = %.1lf\n", a, b, a * b);
+	 break;
+     case '/':
+	 printf("%.1lf / %.1lf = %.1lf\n", a, b, a / b);
+	 break;
+	 // If operator doesn't match any case constant
+     default:
+	 printf("Error! please write a valid operator\n");
+	}
+	printf("\n");
+    }
+}
+
