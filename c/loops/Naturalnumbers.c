5e98f3401052b1377c33e9d80f09f2c854847d12
KotariVinay <vinaykotari7@gmail.com>
2024-05-16 22:17:01 +0530

    loops

---
+++ c/loops/Naturalnumbers.c
@@ -0,0 +1,19 @@
+/*
+Name : K.Y.Naga Sai Vinay
+Date : 16-05-2024
+Description : WAP to program to print 1 to n natural numbers
+*/
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+	printf("%d ",i);
+    }
+    return 0;
+
+}
+
